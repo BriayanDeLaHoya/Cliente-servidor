04ded41b67bd13c4ea09d8ce460cc2e2eada6d01
BriayanDeLaHoya <sm13ma.hovb980521hdgyzro2@gmail.com>
2018-06-05 09:22:21 -0500

    API version 2

---
+++ Proyectos Pof. Octavio/API/app-verbs.js
@@ -0,0 +1,24 @@
+var express = require("express");
+
+var app = express();
+
+app.get("/",(req, res)=>{
+    res.send("Utilizaste el verbo GET");
+});
+
+app.post("/",(req, res)=>{
+    res.send("Utlizaste el verbo POST");
+});
+
+app.put("/",(req, res)=>{
+    res.status(400).send("Utilizaste el verbo PUT");
+
+
+
+});
+
+app.delete("/",(req, res)=>{
+    res.status(500).send("Utilizaste el verbo DEELETE");
+});
+
+app.listen(3000);
--- Proyectos Pof. Octavio/API/app.js
+++ Proyectos Pof. Octavio/API/app.js
@@ -2,8 +2,11 @@ var express = require("express");
 var app = express();
 
 var api = require("./routes/api");
+var api2 = require("./routes/api2");
+
 
 app.use("/api", api);
+app.use("/apiV2", api2);
 
 app.get("/", (req, res)=>{
     res.send("<h1> Pagina Principal</h1>");
